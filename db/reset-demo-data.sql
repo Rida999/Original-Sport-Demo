3f4da0703427163ee0a68a16348d8e777ac44f57
Rida999 <ridaajam999@gmail.com>
2026-08-11 20:22:05 +0300

    reset behavior fixed

--- db/reset-demo-data.sql
+++ db/reset-demo-data.sql
@@ -1,17 +1,11 @@
+-- Preserve superadmin-managed inventory and import data between demo resets.
+-- Only demo transaction state is restarted every 30 minutes.
 TRUNCATE TABLE receipt_draft CASCADE;
 TRUNCATE TABLE receipt_items CASCADE;
 TRUNCATE TABLE receipts CASCADE;
-TRUNCATE TABLE import_items CASCADE;
-TRUNCATE TABLE import_batches CASCADE;
 TRUNCATE TABLE activity_logs CASCADE;
-TRUNCATE TABLE products CASCADE;
-TRUNCATE TABLE categories CASCADE;
 
-INSERT INTO categories SELECT * FROM categories_seed;
-INSERT INTO products SELECT * FROM products_seed;
 INSERT INTO activity_logs SELECT * FROM activity_logs_seed;
-INSERT INTO import_batches SELECT * FROM import_batches_seed;
-INSERT INTO import_items SELECT * FROM import_items_seed;
 INSERT INTO receipts SELECT * FROM receipts_seed;
 INSERT INTO receipt_items SELECT * FROM receipt_items_seed;
 INSERT INTO receipt_draft SELECT * FROM receipt_draft_seed;
